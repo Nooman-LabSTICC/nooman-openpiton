dbbc63adafa8273d7650ea1e4ecc3ec468043a27
Marcelo Ruaro <mcruaro@gmail.com>
2021-11-24 12:43:39 +0100

    Update log file path

--- piton/design/chip/tile/ariane/src/perf_counters.sv
+++ piton/design/chip/tile/ariane/src/perf_counters.sv
@@ -136,15 +136,15 @@ module perf_counters #(parameter int unsigned CORE_ADDR = 1)(
         if (commit_ack_i[i]) begin
           //******Intruction Counter********
           if (commit_instr_i[i].fu == ALU) begin
-            fd = $fopen("mem_cpu_log.txt", "a");
+            fd = $fopen("debug/mem_cpu_log.txt", "a");
             $fdisplay(fd,"ALUinstr:%0d:%0d",counter, CORE_ADDR);
             $fclose(fd); 
           end else if (commit_instr_i[i].fu == LOAD || commit_instr_i[i].fu == STORE) begin
-            fd = $fopen("mem_cpu_log.txt", "a");
+            fd = $fopen("debug/mem_cpu_log.txt", "a");
             $fdisplay(fd,"LSUinstr:%0d:%0d",counter,CORE_ADDR);
             $fclose(fd); 
           end else begin
-            fd = $fopen("mem_cpu_log.txt", "a");
+            fd = $fopen("debug/mem_cpu_log.txt", "a");
             $fdisplay(fd,"Othersinstr:%0d:%0d",counter,CORE_ADDR);
             $fclose(fd); 
           end
