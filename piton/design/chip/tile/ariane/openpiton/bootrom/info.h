5550f03b43697e2c342a6d44d8df257d93805513
Marcelo Ruaro <mcruaro@gmail.com>
2021-11-24 14:36:53 +0100

    Add bootroom

---
+++ piton/design/chip/tile/ariane/openpiton/bootrom/info.h
@@ -0,0 +1,29 @@
+// Info string generated with get_bootrom_info(...)
+// OpenPiton + Ariane framework
+// Date: Nov 24 2021 13:00:21
+
+const char info[] = {
+"\r\n\r\n"
+"----------------------------------------\r\n"
+"--     OpenPiton+Ariane Platform      --\r\n"
+"----------------------------------------\r\n"
+"OpenPiton Version: 9d5200c0                   \r\n"
+"Ariane Version:    9d5200c0                   \r\n"
+"                                        \r\n"
+"FPGA Board:        None (Simulation)                   \r\n"
+"Build Date:        Nov 24 2021 13:00:21                   \r\n"
+"                                        \r\n"
+"#X-Tiles:          3                   \r\n"
+"#Y-Tiles:          3                   \r\n"
+"#Cores:            9                   \r\n"
+"Core Freq:         Unknown                   \r\n"
+"Network:           2d_mesh                   \r\n"
+"DRAM Size:         1024 MB                \r\n"
+"                                        \r\n"
+"L1I Size / Assoc:   16 kB / 4          \r\n"
+"L1D Size / Assoc:    8 kB / 4          \r\n"
+"L15 Size / Assoc:    8 kB / 4          \r\n"
+"L2  Size / Assoc:   64 kB / 4          \r\n"
+"----------------------------------------\r\n\r\n\r\n"
+};
+
